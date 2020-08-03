95e688c8643a741e040d5f84b87429004039f3b1
royc67 <royc67@gmail.com>
2020-08-03 18:25:29 +0300

    added types

--- main.js
+++ main.js
@@ -32,13 +32,22 @@ const createContainer = (data) => {
   const tempPoke = [document.createElement('div') , 
                     document.createElement('div') , 
                     document.createElement('div'), 
-                    document.createElement('img')];
+                    document.createElement('img'),
+                    document.createElement('ul')];
   tempPoke[0].innerHTML = `name: ${data.name}`; 
   tempPoke[1].innerHTML = `height: ${data.height}`; 
   tempPoke[2].innerHTML = `weight: ${data.weight}`; 
   tempPoke[3].src = data.sprites.front_default;
   tempPoke[3].addEventListener('mouseover', ()=> {tempPoke[3].src = data.sprites.back_default});
   tempPoke[3].addEventListener('mouseleave', ()=> {tempPoke[3].src = data.sprites.front_default});
+
+  //types:
+  const types = []
+  for (let i = 0; i< data.types.length; i++){
+    let temp = document.createElement('li')
+    temp.innerHTML = data.types[i].type.name;
+    tempPoke[4].appendChild(temp);
+  }
 
   tempPoke.forEach(element => {displayDiv.appendChild(element)});
   
