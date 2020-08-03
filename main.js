c5b4ce48de94de8a79f59f2f23f4285231d1de04
royc67 <royc67@gmail.com>
2020-08-03 14:39:51 +0300

    Elements declaration

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const searchInput = document.getElementById('search');
+const searchButton = document.getElementById('searchButon');
+const displayDiv = document.getElementById('results');
+
+
 const searchPokemon = async (pokemonId = 3) => {
   const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
   console.log(data)
